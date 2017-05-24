0e7cc4eb413c311bf4a690a126f0bbbc90aebcfd
Will Keung <williamk212@gmail.com>
2017-05-24 01:45:00 -0400

    sample of good main

---
+++ sample/my_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+
+def main(args=None):
+    """main routine"""
+    if args is None:
+        args = sys.argv[1:]
+
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
+
